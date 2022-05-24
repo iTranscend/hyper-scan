e09c1dcd27fda4f1d6f30439af6ba17c7bc8508a
iTranscend <llordbanks500@gmail.com>
2022-05-24 15:42:20 +0100

    feat: accept and scan custom port range

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::net::{SocketAddr, TcpStream, ToSocketAddrs};
+use std::num::NonZeroUsize;
 use std::process::Command;
 use structopt::StructOpt;
 
@@ -8,15 +9,34 @@ use structopt::StructOpt;
 struct Opts {
     #[structopt(short = "h", long = "host", default_value = "localhost")]
     host: String,
+
+    #[structopt(short = "v", long = "verbose", help = "Print verbose output")]
+    verbose: bool,
+
+    #[structopt(short = "t", long = "threads", help = "Number of threads to use")]
+    threads: Option<NonZeroUsize>,
+
+    #[structopt(short = "sp", long = "startPort", help = "Port to start scanning from")]
+    start_port: Option<u32>,
+
+    #[structopt(short = "ep", long = "endPort", help = "Port to end scanning at")]
+    end_port: Option<u32>,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts = Opts::from_args();
-    println!("Host to scan: {:?}", opts.host);
+
+    let available_threads = std::thread::available_parallelism()?;
+    let threads = opts.threads.unwrap_or(available_threads).get();
+
+    if opts.verbose {
+        println!("Host to scan: {:?}", opts.host);
+        println!("Number of threads: {:?}", threads);
+    }
 
     let mut open_ports: Vec<u32> = vec![];
 
-    for port in 0..65535 {
+    for port in opts.start_port.unwrap_or(100)..opts.end_port.unwrap_or(145) {
         let pre_addr = format!("{}:{}", opts.host, port);
         let addr: SocketAddr = pre_addr.to_socket_addrs().unwrap().next().unwrap();
         match TcpStream::connect(&addr) {
@@ -36,4 +56,6 @@ fn main() {
         let stdout = String::from_utf8_lossy(&output.stdout);
         println!("{}", stdout);
     }
+
+    Ok(())
 }
